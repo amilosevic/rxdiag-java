e4125a34e749140d2cfb38004172b5417dc83735
Aleksandar Milosevic <milosevic.aca@gmail.com>
2015-05-30 21:10:32 +0200

    initial project structure

---
+++ src/main/java/amilosevic/example/RxDiag.java
@@ -0,0 +1,10 @@
+package amilosevic.example;
+
+/**
+ * Rx Diag Java version.
+ */
+public class RxDiag {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/amilosevic/example/RxDiagTest.java
@@ -0,0 +1,34 @@
+package amilosevic.example;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class RxDiagTest
+        extends TestCase {
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public RxDiagTest(String testName) {
+        super(testName);
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite() {
+        return new TestSuite(RxDiagTest.class);
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp() {
+        assertTrue(true);
+    }
+}
